b8a4087f528a935e3379fd6268fe398db83d54a2
madebyskippy <timxsun@gmail.com>
2020-12-26 18:23:02 -0500

    user controls with arrow keys

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,25 +6,47 @@ using UnityEngine.UI;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] Transform center;
-    [SerializeField] float radius;
+    [SerializeField] float baseRadius;
+    [SerializeField] Vector2 radiusRange;
+    [SerializeField] float easing;
     [SerializeField] float speed;
     [SerializeField] Text text;
 
     float angle;
+    float radius;
+    float radiusTarget;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        radius = baseRadius;
+        radiusTarget = baseRadius;
     }
 
     // Update is called once per frame
     void Update()
-    {
+    { 
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            radiusTarget = radiusRange.y;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            radiusTarget = radiusRange.x;
+        }
+        if (!Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
+        {
+            radiusTarget = baseRadius;
+        }
+
+        radius = Mathf.Lerp(radius, radiusTarget, easing);
+
         angle = (angle + 1 * speed * Time.deltaTime) % 360;
-        text.text = angle.ToString() + " " + Mathf.Cos(Mathf.Deg2Rad*angle);
+        text.text = angle.ToString();
         Vector2 pos;
-        pos.x = center.position.x + radius * Mathf.Cos(Mathf.Deg2Rad*angle);
-        pos.y = center.position.y + radius * Mathf.Sin(Mathf.Deg2Rad*angle);
+        pos.x = center.position.x + radius * Mathf.Cos(Mathf.Deg2Rad * angle);
+        pos.y = center.position.y + radius * Mathf.Sin(Mathf.Deg2Rad * angle);
         this.transform.position = new Vector3(pos.x, 0, pos.y);
+
     }
 }
